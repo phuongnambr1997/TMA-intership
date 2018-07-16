0ed8438313037ebe6c863f1ac8f3c06eaddeac60
Nam Nguyen Phuong <intern.npnam@tma.com.vn>
2018-07-16 10:34:30 +0700

    Project Mobile Shop

---
+++ Project/shoppingbackend/src/main/java/com/shoppingbackend/dao/UserDAO.java
@@ -0,0 +1,26 @@
+package com.shoppingbackend.dao;
+
+import java.util.List;
+
+import com.shoppingbackend.dto.Address;
+import com.shoppingbackend.dto.Cart;
+import com.shoppingbackend.dto.User;
+
+public interface UserDAO {
+
+	// user related operation
+	User getByEmail(String email);
+	User get(int id);
+
+	boolean add(User user);
+	
+	// adding and updating a new address
+	Address getAddress(int addressId);
+	boolean addAddress(Address address);
+	boolean updateAddress(Address address);
+	Address getBillingAddress(int userId);
+	List<Address> listShippingAddresses(int userId);
+	
+
+	
+}
